d54dc3949554118094ec08ab08696e34ae107d98
DragonEmperorG <372972084@qq.com>
2018-08-18 14:44:38 +0800

    Add prediction Re code

---
+++ data_processing/make_prediction_dataset.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+@author: LONG QIAN
+"""
+ 
+import os 
+import sys
+import csv
+import configparser
+import numpy as np
+import tensorflow as tf 
+
+from PIL import Image
+from tqdm import tqdm
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def make_dataset():
+    cwd = os.getcwd()
+    raw_data_path = os.path.join(cwd, 'data_processing','PredictionJPG')
+    # 要生成的文件
+    predition_writer = tf.python_io.TFRecordWriter(os.path.join(cwd, 'dataset', "ion_prediction.tfrecords"))
+    csv_reader = csv.reader(open(os.path.join(cwd, 'data_processing', "PreExogenous.txt")))
+    conf = configparser.ConfigParser()
+
+    row_list = []
+    for row in csv_reader:
+        row.pop(0)
+        row_list.append(row)
+    
+    ext_data_np = np.array(row_list, dtype=np.float)
+    # print(ext_data_np.shape)
+    
+    input_time_steps = 36
+    output_time_steps = 12
+    iot_time_steps = input_time_steps + output_time_steps
+
+    # 初始化图片序列处理的相关变量
+    time_step_counter    = 0    # 时间戳记录变量
+    nb_predition_samples = 0    # 预测集样本统计变量
+    # total_time_step        = len(os.listdir(raw_data_path))    # 时间戳总量
+
+    # 样本构建滑动窗口变量初始化
+    iot_img_sequences      = []
+    input_img_sequences    = []
+    output_img_sequences   = []
+
+    iot_time_sequences     = []  
+    input_time_sequences   = []
+    output_time_sequences  = []
+
+    iot_ext_sequences      = []
+    input_ext_sequences    = []
+
+    for img_name in tqdm(os.listdir(raw_data_path), desc="TFRecordWriterProgress", unit="image", ascii=True):
+        # 每一个图片的地址
+        img_path = os.path.join(raw_data_path, img_name)
+
+        if(not os.path.exists(img_path)):
+            continue
+
+        img = Image.open(img_path)
+        # 将图片转化为矩阵，并获取当前图片的时间戳
+        img_raw_array = np.array(img)
+        img_raw_array = np.reshape(img_raw_array, (img_raw_array.shape[0], img_raw_array.shape[1], 1))
+        img_time_step = int(os.path.splitext(img_name)[0])
+
+        iot_img_sequences.append(img_raw_array)
+        iot_time_sequences.append(img_time_step)
+        iot_ext_sequences.append(ext_data_np[time_step_counter,:])
+
+        time_step_counter = time_step_counter + 1
+
+        # 当滑动窗口内的样本达到一个输入输出量的时候开始生成样本
+        if (time_step_counter >= iot_time_steps):
+            # 分离输入输出样本
+            input_img_sequences   = iot_img_sequences[:input_time_steps]
+            output_img_sequences  = iot_img_sequences[input_time_steps:]
+            input_time_sequences  = iot_time_sequences[:input_time_steps]
+            output_time_sequences = iot_time_sequences[input_time_steps:]
+            input_ext_sequences   = iot_ext_sequences[:input_time_steps]
+
+            input_img_sequences_array   = np.array(input_img_sequences)
+            output_img_sequences_array  = np.array(output_img_sequences)
+            input_time_sequences_array  = np.array(input_time_sequences)
+            output_time_sequences_array = np.array(output_time_sequences)
+            input_ext_sequences_array   = np.array(input_ext_sequences)
+
+            # print(input_img_sequences_array.shape)
+            # print(input_ext_sequences_array.shape)
+
+            # example对象对input_img_sequences、input_ext_sequences和output_img_sequences等e数据进行封装
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'input_img_sequences'        : _bytes_feature([input_img_sequences_array.tobytes()]),
+                'output_img_sequences'       : _bytes_feature([output_img_sequences_array.tobytes()]),
+                'input_time_sequences'       : _int64_feature(input_time_sequences_array.flatten().tolist()),
+                'output_time_sequences'      : _int64_feature(output_time_sequences_array.flatten().tolist()),
+                'input_ext_sequences'        : _float_feature(input_ext_sequences_array.flatten().tolist()),
+                'input_img_sequences_shape'  : _int64_feature(input_img_sequences_array.shape),
+                'output_img_sequences_shape' : _int64_feature(output_img_sequences_array.shape),
+                'input_ext_sequences_shape'  : _int64_feature(input_ext_sequences_array.shape),
+            }))
+
+            nb_predition_samples = nb_predition_samples + 1
+            predition_writer.write(example.SerializeToString())
+            
+
+            iot_img_sequences.pop(0)
+            iot_time_sequences.pop(0)
+            iot_ext_sequences.pop(0)
+    
+    predition_writer.close()
+
+    # 统计回写数据集的相关参数
+    conf["DatasetInfo"] = {        
+        'nb_time_step'         : time_step_counter,
+        'nb_prediction_samples' : nb_predition_samples,
+    }
+    with open(os.path.join(cwd, 'dataset', 'ion_dataset_prediction_info.ini'), 'w') as configfile:
+        conf.write(configfile)
+
+
+def main():
+    make_dataset()
+
+if  __name__ == '__main__':
+    main()
---
+++ predict.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+@author: LONG QIAN
+"""
+
+
+import os 
+import configparser
+import numpy as np
+import tensorflow as tf 
+
+from datetime import datetime
+from tqdm import tqdm
+# from PIL import Image
+from scipy.io import savemat
+from model.params import Params
+from model.tec_pre_net import tec_pre_net
+
+
+def parse_data(serialized_example):
+    #return input_img_sequences and output_img_sequences
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'input_img_sequences'        : tf.FixedLenFeature([], tf.string),
+            'output_img_sequences'       : tf.FixedLenFeature([], tf.string),
+            'output_time_sequences'      : tf.FixedLenFeature([Params.output_time_steps], tf.int64),
+            'input_ext_sequences'        : tf.FixedLenFeature([180], tf.float32),
+            'input_img_sequences_shape'  : tf.FixedLenFeature([4], tf.int64),
+            'output_img_sequences_shape' : tf.FixedLenFeature([4], tf.int64),
+            'input_ext_sequences_shape'  : tf.FixedLenFeature([2], tf.int64),
+        }
+    )
+
+    input_img_sequences = tf.decode_raw(features['input_img_sequences'], tf.uint8)
+    output_img_sequences = tf.decode_raw(features['output_img_sequences'], tf.uint8)
+
+    output_time_sequences      = tf.cast(features['output_time_sequences'], tf.int32)
+    input_img_sequences_shape  = tf.cast(features['input_img_sequences_shape'], tf.int32)
+    output_img_sequences_shape = tf.cast(features['output_img_sequences_shape'], tf.int32)
+    input_ext_sequences_shape  = tf.cast(features['input_ext_sequences_shape'], tf.int32)
+
+    input_img_sequences = tf.reshape(input_img_sequences, input_img_sequences_shape)
+    output_img_sequences = tf.reshape(output_img_sequences, output_img_sequences_shape)
+    input_ext_sequences = tf.reshape(features['input_ext_sequences'], input_ext_sequences_shape)
+    #throw input_img_sequences tensor
+    # input_img_sequences = tf.cast(input_img_sequences, tf.int32)
+    #throw output_img_sequences tensor
+    # output_img_sequences = tf.cast(output_img_sequences, tf.int32)
+
+    return input_img_sequences, input_ext_sequences, output_img_sequences, output_time_sequences
+
+
+def load_data(filename):
+    # create a queue
+    # filename_queue = tf.train.string_input_producer([filename])
+
+    # reader = tf.TFRecordReader()
+    # return file_name and file
+    # _, serialized_example = reader.read(filename_queue)
+    dataset = tf.data.TFRecordDataset(filename)
+    return dataset.map(parse_data)
+
+    
+if __name__ == '__main__':
+    cwd = os.getcwd()
+    config = configparser.ConfigParser()
+    config.read(os.path.join(cwd, 'dataset', "ion_dataset_prediction_info.ini"))
+
+    img_rows              = Params.map_rows
+    img_cols              = Params.map_cols
+    input_time_steps      = Params.input_time_steps
+    output_time_steps     = Params.output_time_steps
+    # nb_prediction_samples  = 12
+    nb_prediction_samples  = config.getint('DatasetInfo', 'nb_prediction_samples')
+
+    load_weights_path     = os.path.join(cwd, 'checkpoint', '20180815001811', "TEC_PRE_NET_MODEL_WEIGHTS.10-0.01543.hdf5")
+    prediction_save_path  = os.path.join(cwd, 'prediction', datetime.now().strftime('%Y%m%d%H%M%S'))
+    try:
+        os.makedirs(prediction_save_path)
+    except:
+        pass
+
+    prediction_dataset          = load_data(os.path.join(cwd, 'dataset','ion_prediction.tfrecords'))
+    prediction_dataset_iterator = prediction_dataset.make_initializable_iterator()
+    prediction_dataset_iterator_next_element = prediction_dataset_iterator.get_next()
+
+    input_img_sequences_prediction  = []
+    input_ext_sequences_prediction  = []
+    output_img_sequences_prediction = []
+    output_time_sequences_prediction = []
+
+    #开始一个会话
+    # with tf.Session(config=tfconfig) as sess:
+    with tf.Session() as sess:  
+        sess.run(prediction_dataset_iterator.initializer)
+
+        try:
+            while True:
+                input_img_sequences, input_ext_sequences, output_img_sequences, output_time_sequences = sess.run(prediction_dataset_iterator_next_element)
+                input_img_sequences_prediction.append(input_img_sequences)
+                input_ext_sequences_prediction.append(input_ext_sequences)
+                output_img_sequences_prediction.append(output_img_sequences)
+                output_time_sequences_prediction.append(output_time_sequences)
+
+        except tf.errors.OutOfRangeError:
+            print("prediction dataset constructed ...")
+
+    print("Start model construction ...")
+    model = tec_pre_net((img_rows, img_cols))
+    model.load_weights(load_weights_path, by_name=False)
+    print("Model constructed ...")
+
+    for prediction_samples_counter in tqdm(range(nb_prediction_samples), desc="IONPredictionProgress", unit="sequences", ascii=True):
+        input_img_sequences_predict_fit_x = np.expand_dims(input_img_sequences_prediction[prediction_samples_counter], axis=0)
+        input_ext_sequences_predict_fit_x = np.expand_dims(input_ext_sequences_prediction[prediction_samples_counter], axis=0)
+
+        output_img_sequences_predict_raw = model.predict([input_img_sequences_predict_fit_x, input_ext_sequences_predict_fit_x])
+        # output_img_sequences_predict = output_img_sequences_predict_raw.astype(int)
+        output_img_sequences_predict = output_img_sequences_predict_raw
+
+        output_img_sequences_prediction_concatenate    = np.reshape(output_img_sequences_prediction[prediction_samples_counter], (output_time_steps*img_rows, img_cols, 1))
+        output_img_sequences_predict_concatenate = np.reshape(output_img_sequences_predict[0], (output_time_steps*img_rows, img_cols, 1))
+
+        output_img_sequences_concatenated = np.concatenate((output_img_sequences_prediction_concatenate, output_img_sequences_predict_concatenate), axis=1)
+        
+        output_img_sequences_concatenated_2D = np.reshape(output_img_sequences_concatenated, (output_time_steps*img_rows, img_cols+img_cols))
+        output_img_sequences_matlab_name = str(output_time_sequences_prediction[prediction_samples_counter][0]) + '-' + str(output_time_sequences_prediction[prediction_samples_counter][-1]) + ".mat"
+        savemat(
+            os.path.join(prediction_save_path, output_img_sequences_matlab_name),
+            {
+                'output_img_sequences_concatenated': output_img_sequences_concatenated_2D
+            }
+        )
+
+        # output_img_sequences_concatenated_2D_uint8 = output_img_sequences_concatenated_2D.astype(np.uint8)
+        # output_img_sequences_visual = Image.fromarray(output_img_sequences_concatenated_2D_uint8, mode="L")
+        # output_img_sequences_visual_name = str(output_time_sequences_prediction[prediction_samples_counter][0]) + '-' + str(output_time_sequences_prediction[prediction_samples_counter][-1]) + ".jpeg"
+        # output_img_sequences_visual.save(os.path.join(prediction_save_path, output_img_sequences_visual_name))
